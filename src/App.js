893d4daf397b1d444d8d67cb6355f4ae0f7b1449
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2023-12-26 10:43:56 +0100

    Add hands

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "@tensorflow/tfjs";
 // Register WebGL backend.
 import "@tensorflow/tfjs-backend-webgl";
 import "@mediapipe/face_mesh";
+import "@mediapipe/hands";
 import Webcam from "react-webcam";
 import { runDetector, processColor } from "./utils";
 import defaultSetup from "./_setup.json";
@@ -80,90 +81,122 @@ function App() {
         viewBox={`0 0 ${inputResolution.width} ${inputResolution.height}`}
         style={{ position: "absolute", mixBlendMode: setup.blendMode }}
       >
-        {points.slice(0, -setup.transitionArrangement - 1).map((point, index) => (
-          <circle
-            key={index}
-            cx={point.x}
-            cy={point.y}
-            r={Math.max(0, point.z + setup.radius) * setup.growth}
-            stroke="none"
-            fill={processColor(setup.color, setup.opacity)}
-          >
-            {setup.hasTransition && (
-              <>
-                <animate
-                  attributeName="cx"
-                  values={`${points[index].x};${
-                    points[index + setup.transitionArrangement].x
-                  }`}
-                  keyTimes="0;1"
-                  dur={`${setup.transitionDuration}s`}
-                  repeatCount="indefinite"
-                />
-                <animate
-                  attributeName="cy"
-                  values={`${points[index].y};${
-                    points[index + setup.transitionArrangement].y
-                  }`}
-                  keyTimes="0;1"
-                  dur={`${setup.transitionDuration}s`}
-                  repeatCount="indefinite"
-                />
-                <animate
-                  attributeName="r"
-                  values={`${
-                    Math.max(0, point.z + setup.radius) * setup.growth
-                  };${
-                    Math.max(0, points[index + setup.transitionArrangement].z + setup.radius) *
-                    setup.growth
-                  }`}
-                  keyTimes="0;1"
-                  dur={`${setup.transitionDuration}s`}
-                  repeatCount="indefinite"
-                />
-              </>
-            )}
-          </circle>
-        ))}
-        {/* {points.slice(0, -4).map((point, index) => (
-          <line
-            x1={point.x}
-            x2={points[index + setup.transitionArrangement].x}
-            y1={point.y}
-            y2={points[index + setup.transitionArrangement].y}
-            stroke="black"
-            fill="#00000020"
-          >
-            <animate
-              attributeName="x1"
-              values={`${points[index].x};${points[index + setup.transitionArrangement].x}`}
-              keyTimes="0;1"
-              dur={`${setup.transitionDuration}s`}
-              repeatCount="indefinite"
-            />
-            <animate
-              attributeName="y1"
-              values={`${points[index].y};${points[index + setup.transitionArrangement].y}`}
-              keyTimes="0;1"
-              dur={`${setup.transitionDuration}s`}
-              repeatCount="indefinite"
-            />
-            <animate
-              attributeName="x2"
-              values={`${points[index + 2].x};${points[index + 3].x}`}
-              keyTimes="0;1"
-              dur={`${setup.transitionDuration}s`}
-              repeatCount="indefinite"
-            />
-            <animate
-              attributeName="y2"
-              values={`${points[index + 2].y};${points[index + 3].y}`}
-              keyTimes="0;1"
-              dur={`${setup.transitionDuration}s`}
-              repeatCount="indefinite"
-            />
-          </line>
-        ))} */}
+        {points
+          .slice(0, -setup.transitionArrangement - 1)
+          .map((point, index) => (
+            <>
+              {setup.hasCircles && (
+                <circle
+                  key={`c-${index}`}
+                  cx={point.x}
+                  cy={point.y}
+                  r={Math.max(0, point.z + setup.radius) * setup.growth}
+                  stroke="none"
+                  fill={processColor(setup.color, setup.opacity)}
+                >
+                  {setup.hasTransition && (
+                    <>
+                      <animate
+                        attributeName="cx"
+                        values={`${points[index].x};${
+                          points[index + setup.transitionArrangement].x
+                        }`}
+                        keyTimes="0;1"
+                        dur={`${setup.transitionDuration}s`}
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="cy"
+                        values={`${points[index].y};${
+                          points[index + setup.transitionArrangement].y
+                        }`}
+                        keyTimes="0;1"
+                        dur={`${setup.transitionDuration}s`}
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="r"
+                        values={`${
+                          Math.max(0, point.z + setup.radius) * setup.growth
+                        };${
+                          Math.max(
+                            0,
+                            points[index + setup.transitionArrangement].z +
+                              setup.radius
+                          ) * setup.growth
+                        }`}
+                        keyTimes="0;1"
+                        dur={`${setup.transitionDuration}s`}
+                        repeatCount="indefinite"
+                      />
+                    </>
+                  )}
+                </circle>
+              )}
+              {setup.hasLines && (
+                <line
+                  key={`l-${index}`}
+                  x1={point.x}
+                  x2={points[index + setup.transitionArrangement].x}
+                  y1={point.y}
+                  y2={points[index + setup.transitionArrangement].y}
+                  stroke={processColor(setup.color, setup.opacity)}
+                  strokeWidth={setup.radius + point.z * setup.growth}
+                >
+                  {setup.hasTransition && (
+                    <>
+                      <animate
+                        attributeName="x1"
+                        values={`${points[index].x};${
+                          points[index + setup.transitionArrangement].x
+                        }`}
+                        keyTimes="0;1"
+                        dur={`${setup.transitionDuration}s`}
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="y1"
+                        values={`${points[index].y};${
+                          points[index + setup.transitionArrangement].y
+                        }`}
+                        keyTimes="0;1"
+                        dur={`${setup.transitionDuration}s`}
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="x2"
+                        values={`${
+                          points[index + setup.transitionArrangement].x
+                        };${points[index].x}`}
+                        keyTimes="0;1"
+                        dur={`${setup.transitionDuration}s`}
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="y2"
+                        values={`${
+                          points[index + setup.transitionArrangement].y
+                        };${points[index].y}`}
+                        keyTimes="0;1"
+                        dur={`${setup.transitionDuration}s`}
+                        repeatCount="indefinite"
+                      />
+                    </>
+                  )}
+                </line>
+              )}
+              {setup.hasNumbers && (
+                <text
+                  key={`t-${index}`}
+                  x={point.x}
+                  y={point.y}
+                  fill={processColor(setup.color, setup.opacity)}
+                >
+                  {index}
+                </text>
+              )}
+            </>
+          ))}
       </svg>
       <Menu
         {...{
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,42 @@
-import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+// import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+import * as handPoseDetection from "@tensorflow-models/hand-pose-detection";
 
 export const runDetector = async (video, setPoints) => {
-  const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+  // const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+  // const detectorConfig = {
+  //   runtime: "tfjs",
+  //   refineLandmarks: true
+  // };
+  // const detector = await faceLandmarksDetection.createDetector(
+  //   model,
+  //   detectorConfig
+  // );
+  // const detect = async (net) => {
+  //   const estimationConfig = { flipHorizontal: true };
+  //   const faces = await net.estimateFaces(video, estimationConfig);
+  //   requestAnimationFrame(() => {
+  //     if (faces && faces[0] && faces[0].keypoints) {
+  //       setPoints(faces[0].keypoints);
+  //     }
+  //   });
+  //   detect(detector);
+  // };
+  // detect(detector);
+  const model = handPoseDetection.SupportedModels.MediaPipeHands;
   const detectorConfig = {
-    runtime: "tfjs",
-    refineLandmarks: true
+    runtime: "tfjs", // or 'tfjs',
+    modelType: "full"
   };
-  const detector = await faceLandmarksDetection.createDetector(
+  const detector = await handPoseDetection.createDetector(
     model,
     detectorConfig
   );
   const detect = async (net) => {
     const estimationConfig = { flipHorizontal: true };
-    const faces = await net.estimateFaces(video, estimationConfig);
+    const hands = await net.estimateHands(video, estimationConfig);
     requestAnimationFrame(() => {
-      if (faces && faces[0] && faces[0].keypoints) {
-        setPoints(faces[0].keypoints);
+      if (hands && hands[0] && hands[0].keypoints) {
+        setPoints(hands[0].keypoints);
       }
     });
     detect(detector);
