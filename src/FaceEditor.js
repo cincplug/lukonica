43575543fd73632cbae3fa227c351ec6148c29eb
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2023-12-29 14:00:19 +0100

    Improve face editor usability

--- src/FaceEditor.js
+++ src/FaceEditor.js
@@ -7,6 +7,9 @@ function FaceEditor(props) {
   const { inputResolution, setIsEditing } = props;
   const [areas, setAreas] = useState([]);
   const [activeArea, setActiveArea] = useState([]);
+  const [mouseX, setMouseX] = useState(inputResolution.width / 2);
+  const [mouseY, setMouseY] = useState(inputResolution.height / 2);
+
   const handleDotClick = (_event, pointIndex) => {
     if (activeArea.length === 0) {
       setActiveArea([pointIndex]);
@@ -24,19 +27,30 @@ function FaceEditor(props) {
       }
     }
   };
+  const handleMouseMove = (event) => {
+    setMouseX(event.pageX || event.touches[0].pageX);
+    setMouseY(event.pageY || event.touches[0].pageY);
+  };
+
   return (
-    <div className="face-editor">
+    <div className="face-editor" onMouseMove={handleMouseMove}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox={`0 0 ${inputResolution.width} ${inputResolution.height}`}
       >
+        {activeArea.length > 0 ? (
+          <path
+            className="face-editor__active-area"
+            d={`${renderPath(activeArea)} L${mouseX},${mouseY}`}
+          />
+        ) : null}
         {defaultFacePoints.map((point, pointIndex) => {
           return (
             <circle
               key={`c-${pointIndex}`}
               cx={point.x}
               cy={point.y}
-              r={2}
+              r={4}
               className={`face-editor__dot ${
                 activeArea[0] === pointIndex ? "face-editor__dot--first" : ""
               }`}
@@ -44,12 +58,6 @@ function FaceEditor(props) {
             />
           );
         })}
-        {activeArea.length > 0 ? (
-          <path
-            className="face-editor__active-area"
-            d={renderPath(activeArea)}
-          />
-        ) : null}
 
         {areas.map((area, areaIndex) => (
           <path
