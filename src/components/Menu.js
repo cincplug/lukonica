c0f1d5e1bdb85b2440e9be1cb14c41e775438bbb
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-01-15 09:33:13 +0100

    Fix buttons width

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,49 +3,48 @@ import ControlGroup from "./ControlGroup";
 import { saveSvg, saveJson } from "../utils";
 
 const Menu = (props) => {
-  const {
-    setup,
-    handleInputChange,
-    mask,
-  } = props;
+  const { setup, handleInputChange, mask } = props;
 
   return (
     <>
-      <nav
-        id="mainNav"
-        className={`menu menu--controls`}
-      >
+      <nav id="mainNav" className={`menu menu--controls`}>
         <ControlGroup
           {...{ setup, handleInputChange }}
           controls={defaultSetup.filter(
             (control) => !control.isHidden && !control.isRight
           )}
         />
-        <button
-          className="control__input control__button control__button--save"
-          onClick={() => {
-            saveSvg();
-          }}
-        >
-          Save SVG
-        </button>
-        <button
-          className="control__input control__button control__button--save"
-          onClick={() => {
-            saveJson(mask);
-          }}
-        >
-          Save mask
-        </button>
-        <button
-          className="control__input control__button control__button--clear"
-          onClick={() => {
-            sessionStorage.clear();
-            window.location.reload();
-          }}
-        >
-          Reset
-        </button>
+        <fieldset className="control">
+          <button
+            className="control__input control__button control__button--save"
+            onClick={() => {
+              saveSvg();
+            }}
+          >
+            Save SVG
+          </button>
+        </fieldset>
+        <fieldset className="control">
+          <button
+            className="control__input control__button control__button--save"
+            onClick={() => {
+              saveJson(mask);
+            }}
+          >
+            Save mask
+          </button>
+        </fieldset>
+        <fieldset className="control">
+          <button
+            className="control__input control__button control__button--clear"
+            onClick={() => {
+              sessionStorage.clear();
+              window.location.reload();
+            }}
+          >
+            Reset
+          </button>
+        </fieldset>
       </nav>
     </>
   );
