d26b0d3b3a3307788784efc711ca01bffea11fc7
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-01-31 16:58:10 +0100

    Add watermelons

--- src/components/patterns/path-strokes/index.js
+++ src/components/patterns/path-strokes/index.js
@@ -2,10 +2,13 @@ const pathStrokes = (props) => {
   const { pathStroke, thisPoint, controlPoint, radius, growth } = props;
   const strokeDefinitions = {
     lines: `L${thisPoint.x},${thisPoint.y} `,
-    arcs: `A${radius * growth},${radius * growth} 1 0 1 ${thisPoint.x},${
+    arcs: `A${radius * growth},${radius * growth} 0 0 1 ${thisPoint.x},${
       thisPoint.y
     }`,
-    quadratics: `Q${controlPoint.x},${controlPoint.y} ${thisPoint.x},${thisPoint.y}`
+    quadratics: `Q${controlPoint.x},${controlPoint.y} ${thisPoint.x},${thisPoint.y}`,
+    hairs: `L${controlPoint.x},${controlPoint.y} A${radius * growth},${
+      radius * growth
+    } 1 0 1 ${thisPoint.x},${thisPoint.y}`
   };
   return strokeDefinitions[pathStroke];
 };
