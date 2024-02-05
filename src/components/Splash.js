d34ec883acd5249d26a3ca75e1573b8c1eee26e7
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-02-05 17:16:57 +0100

    Add logo

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,21 +1,27 @@
+import logo from "../assets/lukonica-logo.svg";
 const Splash = (props) => {
   const { setIsEditing, handlePlayButtonClick } = props;
   return (
     <div className="wrap splash">
-      <button
-        className="splash-button"
-        onClick={() => {
-          setIsEditing(true);
-        }}
-      >
-        Create mask
+      <button onClick={handlePlayButtonClick}>
+        <h1>
+          Lukonica
+        </h1>
+        <img src={logo} alt="logo" />
+        <h2>
+          Start video
+        </h2>
       </button>
-      <button
-        className="splash-button video-button play-button"
-        onClick={handlePlayButtonClick}
-      >
-        Start video
-      </button>
+      <div className="buttons">
+        <button
+          className="splash-button"
+          onClick={() => {
+            setIsEditing(true);
+          }}
+        >
+          Create mask
+        </button>
+      </div>
     </div>
   );
 };
