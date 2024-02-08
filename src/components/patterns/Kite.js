1e2dcc4f83e355260594e82df8f206256b4a53fb
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-02-08 09:04:06 +0100

    Update kite

--- src/components/patterns/Kite.js
+++ src/components/patterns/Kite.js
@@ -8,7 +8,8 @@ const Sentence = ({ scribble, scribbleNewArea, setup, radius, growth }) => {
     color,
     opacity,
     usesCssAnimation,
-    transitionDuration
+    transitionDuration,
+    transitionArrangement
   } = setup;
 
   const textArray = Array.from(text);
@@ -16,36 +17,40 @@ const Sentence = ({ scribble, scribbleNewArea, setup, radius, growth }) => {
     .flat()
     .slice(-Math.min(textLimit, textArray.length));
 
-  let shiftedTextArray = textArray;
-  if (area.length >= textArray.length) {
-    shiftedTextArray = textArray
-      .slice(area.length % textArray.length)
-      .concat(textArray.slice(0, area.length % textArray.length));
-  }
-
-  const pathData = area.map((point, index) => {
-    return `${index === 0 ? "M" : "L"} ${point.x} ${point.y}`;
-  }).join(" ");
+  const pathData = area
+    .map((point, index) => {
+      return `${index === 0 ? "M" : "L"} ${point.x} ${point.y}`;
+    })
+    .join(" ") + " z";
 
   return (
     <svg>
       <path id="text-path" d={pathData} fill="none" />
-      {shiftedTextArray.map((letter, index) => {
+      {textArray.map((letter, index) => {
         const style = usesCssAnimation
           ? {
-              animation: `move-to ${transitionDuration}s linear infinite`,
-              animationDelay: `${-index * transitionDuration}s`
+              animation: `move-to ${transitionDuration}s linear infinite`
             }
           : null;
+        const endOffset = (index / textArray.length) * 100;
         return (
           <text
             className={`number-mask`}
             fill={processColor(color, opacity)}
             key={`sent-${index}`}
-            fontSize={radius + index * growth}
+            fontSize={radius * growth + index}
             style={style}
           >
-            <textPath href="#text-path" startOffset={`${(index / shiftedTextArray.length) * 100}%`}>
+            <textPath href="#text-path" startOffset={`${endOffset}%`}>
+              {transitionArrangement > 0 && (
+                <animate
+                  attributeName="startOffset"
+                  from={`${endOffset}%`}
+                  to={`${endOffset + transitionArrangement * radius}%`}
+                  dur={transitionDuration * 10}
+                  repeatCount="indefinite"
+                />
+              )}
               {letter}
             </textPath>
           </text>
