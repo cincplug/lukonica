c65585fa5cbe40b2f4d53ebf50ae5590883a1183
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-02-14 21:15:42 +0100

    Add missing key

--- src/components/Scribble.js
+++ src/components/Scribble.js
@@ -48,43 +48,43 @@ const Scribble = ({ scribble, scribbleNewArea, setup }) => {
       {[...scribble, scribbleNewArea].map((scribbleArea, scribbleAreaIndex) => {
         const fullArea = scribbleArea.flat().reverse();
         const area = fullArea.slice(-Math.min(textLimit, textArray.length));
-        const pathData = area
-          .map((point, index) => {
-            const lastPoint = area[area.length - 1] || point;
-            const prevPoint = area[Math.max(0, index - 1)] || point;
-            const nextPoint = area[Math.min(index + 1, area.length - 1)];
-            if (!point || !lastPoint) return null;
-            if (radius > 0 && lastPoint && index > 0) {
-              const deltaX = point.x - lastPoint.x;
-              const deltaY = point.y - lastPoint.y;
-              const h = Math.hypot(deltaX, deltaY) + radius;
-              const controlPoint = {
-                x: lastPoint.x + deltaX / 2 + deltaY / h,
-                y: lastPoint.y + deltaY / 2 - (radius * deltaX) / h
-              };
-              return pathStrokes({
-                pathStroke: pathStroke,
-                thisPoint: point,
-                prevPoint,
-                nextPoint,
-                controlPoint,
-                radius,
-                growth
-              });
-            } else {
-              return `${index === 0 ? "M" : "L"} ${point.x},${point.y}`;
-            }
-          })
-          .join(" ");
+        const pathData =
+          area
+            .map((point, index) => {
+              const lastPoint = area[area.length - 1] || point;
+              const prevPoint = area[Math.max(0, index - 1)] || point;
+              const nextPoint = area[Math.min(index + 1, area.length - 1)];
+              if (!point || !lastPoint) return null;
+              if (radius > 0 && lastPoint && index > 0) {
+                const deltaX = point.x - lastPoint.x;
+                const deltaY = point.y - lastPoint.y;
+                const h = Math.hypot(deltaX, deltaY) + radius;
+                const controlPoint = {
+                  x: lastPoint.x + deltaX / 2 + deltaY / h,
+                  y: lastPoint.y + deltaY / 2 - (radius * deltaX) / h
+                };
+                return pathStrokes({
+                  pathStroke: pathStroke,
+                  thisPoint: point,
+                  prevPoint,
+                  nextPoint,
+                  controlPoint,
+                  radius,
+                  growth
+                });
+              } else {
+                return `${index === 0 ? "M" : "L"} ${point.x},${point.y}`;
+              }
+            })
+            .join(" ") + (setup.isAutoClosed && area.length > 0 ? " Z" : "");
         return (
-          <>
+          <React.Fragment key={`scr-${scribbleAreaIndex}`}>
             <path
               id={`text-path-${scribbleAreaIndex}`}
               className="scribble"
               fill="none"
               stroke={processColor(color, opacity)}
               strokeWidth={radius * growth}
-              key={`scr-${scribbleAreaIndex}`}
               d={pathData} // Use pathData from Train component
             ></path>
             {text && (
@@ -107,7 +107,7 @@ const Scribble = ({ scribble, scribbleNewArea, setup }) => {
                 </textPath>
               </text>
             )}
-          </>
+          </React.Fragment>
         );
       })}
     </>
