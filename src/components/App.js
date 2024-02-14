1389579ef38f0a427ab83501612cefedf3d854d0
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-02-14 16:55:04 +0100

    Add cloud and watermelon scenario

--- src/components/App.js
+++ src/components/App.js
@@ -81,7 +81,7 @@ const App = () => {
         nextSetup[id] = ["number", "range"].includes(type) ? value / 1 : value;
       }
       sessionStorage.setItem(storageSetupItem, JSON.stringify(nextSetup));
-      console.info(nextSetup);
+      // console.info(nextSetup);
       return nextSetup;
     });
   };
