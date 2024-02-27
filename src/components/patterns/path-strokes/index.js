dea3699d59ac07a1892aa74322206af6bcf906b4
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-02-27 08:57:36 +0100

    Improve scenarios

--- src/components/patterns/path-strokes/index.js
+++ src/components/patterns/path-strokes/index.js
@@ -15,7 +15,7 @@ const pathStrokes = (props) => {
     } ${(thisPoint.y + prevPoint.y) / 2}`,
     watermelons: `L${controlPoint.x} ${controlPoint.y} A${radius * growth} ${
       radius * growth
-    } 1 0 1 ${thisPoint.x} ${thisPoint.y}`
+    } 1 0 1 ${thisPoint.x} ${thisPoint.y} Z`
   };
   return strokeDefinitions[pathStroke];
 };
